14e41f274bb4837b4057e25fbfc195409d00d848
BLAKPC\blak <zhiltsov.nick@gmail.com>
2022-08-23 14:07:38 +0300

    fix: adding stg gateway

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -56,4 +56,6 @@ const isProdGateway = () => {
 
 export const GATEWAY_URL =
   process.env.REACT_APP_GATEWAY_URL ||
-  (IS_PRODUCTION || isProdGateway() ? 'https://gateway.safe.astar.network' : 'https://gateway.safe.astar.network')
+  (IS_PRODUCTION || isProdGateway()
+    ? 'https://gateway.safe.astar.network'
+    : 'https://gateway.staging-safe.astar.network')
